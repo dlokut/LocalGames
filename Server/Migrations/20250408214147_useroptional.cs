b10c9f24762d0e6688e8d034d8a28f0ca81c2e60
dlokut <dlokutijevski8@gmail.com>
2025-04-08 22:56:43 +0100

    added migrations for db changes

---
+++ Server/Migrations/20250408214147_useroptional.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class useroptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GameId",
+                table: "Games",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CommentId",
+                table: "Comments",
+                newName: "tId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlaytimeMins",
+                table: "Playtimes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicFileName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundPicFileName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlaytimeMins",
+                table: "Playtimes");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Games",
+                newName: "GameId");
+
+            migrationBuilder.RenameColumn(
+                name: "tId",
+                table: "Comments",
+                newName: "CommentId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicFileName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BackgroundPicFileName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
