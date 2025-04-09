cc957e0e777d7810b4cd2a9610e03e985104de23
dlokut <dlokutijevski8@gmail.com>
2025-04-09 02:23:42 +0100

    Added method to create admin role

--- Server/Program.cs
+++ Server/Program.cs
@@ -32,4 +32,32 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//Get the role manager to use in create roles method
+var roleManager = app.Services.CreateScope()
+    .ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+await CreateAllRolesAsync(roleManager);
+
 app.Run();
+
+
+async Task CreateAllRolesAsync(RoleManager<IdentityRole> roleManager)
+{
+    await CreateRoleAsync(roleManager, "Admin");
+}
+
+async Task CreateRoleAsync(RoleManager<IdentityRole> roleManager, string roleName)
+{
+    if (await roleManager.RoleExistsAsync(roleName)) return;
+
+    var newRole = new IdentityRole(roleName);
+    var createResult = await roleManager.CreateAsync(newRole);
+
+    if (!createResult.Succeeded)
+    {
+        foreach (var error in createResult.Errors)
+        {
+            app.Logger.LogError($"Error when creating role {roleName} - {error.Description}");
+        }
+    }
+}
