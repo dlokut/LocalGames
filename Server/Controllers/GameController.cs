a716bc46387ccfb4f43ed4d50e1fdcc552c1c0e4
dlokut <dlokutijevski8@gmail.com>
2025-04-10 03:24:20 +0100

    Added igdb package, igdb manager and game controller with test method

---
+++ Server/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : Controller
+    {
+        private readonly IgdbManager igdbManager;
+
+        public GameController()
+        {
+            string? clientId = Environment.GetEnvironmentVariable("IGDB_CLIENT_ID");
+            string? clientSecret = Environment.GetEnvironmentVariable("IGDB_CLIENT_SECRET");
+
+            if (clientId == null || clientSecret == null)
+            {
+                throw new Exception("Client id or client secret is not set");
+            }
+
+            igdbManager = new IgdbManager(clientId, clientSecret);
+        }
+
+        [HttpGet]
+        [Route("v1/GetGameId")]
+        public async Task<ActionResult> GetGameIdAsync(string name)
+        {
+            long? gameId = await igdbManager.GetGameIdAsync(name);
+
+            return Ok(gameId);
+        }
+    }
+}
---
+++ Server/IgdbManager.cs
@@ -0,0 +1,37 @@
+﻿using IGDB;
+using System.Threading.Tasks;
+using IgdbGame = IGDB.Models.Game;
+
+namespace Server
+{
+    public class IgdbManager
+    {
+        private readonly IGDBClient igdbClient;
+
+        public IgdbManager(string clientId, string clientSecret)
+        {
+            igdbClient = new IGDBClient(
+                clientId,
+                clientSecret
+            );
+        }
+
+        public async Task<long?> GetGameIdAsync(string gameName)
+        {
+            IgdbGame[] foundGames = await igdbClient.QueryAsync<IgdbGame>(IGDBClient.Endpoints.Games, 
+                query: $"search \"{gameName}\"; fields id;");
+
+            if (foundGames.Length == 0)
+            {
+                return null;
+            }
+
+            IgdbGame foundGame = foundGames.First();
+
+            return foundGame.Id.Value;
+
+        }
+
+        
+    }
+}
