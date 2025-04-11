e01b5c4b1431d055cf16a2cf84018e03120ed838
dlokut <dlokutijevski8@gmail.com>
2025-04-11 02:07:52 +0100

    Created apply game metadata, made game discovery service use timer

--- Server/Services/GameDiscoveryService.cs
+++ Server/Services/GameDiscoveryService.cs
@@ -2,31 +2,54 @@ using Server.Database;
 
 namespace Server.Services;
 
-public class GameDiscoveryService : BackgroundService, IDisposable
+public class GameDiscoveryService : BackgroundService
 {
+    private IgdbManager igdbManager;
+
+    private Timer? serviceTimer = null;
+    
     private const string GAMES_DIR = "Games";
+
+    private const int TIMER_INTERVAL_MS = 1 * 1000;
+
+    private const int TIMER_START_DELAY_MS = 0;
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            List<GameFile> games = GetGameFiles(GAMES_DIR + "/TestGame");
+        string igdbClientId = Environment.GetEnvironmentVariable("IGDB_CLIENT_ID");
+        string igdbClientSecret = Environment.GetEnvironmentVariable("IGDB_SECRET_ID");
 
-            foreach (GameFile game in games)
-            {
-                Console.WriteLine(game.Directory);
-            }
-            
-            await Task.Delay(1000);
-        }
+        igdbManager = new IgdbManager(igdbClientId, igdbClientSecret);
+
+        serviceTimer = new Timer(ScanGamesDirectory, null,
+            TIMER_START_DELAY_MS, TIMER_INTERVAL_MS);
     }
 
-    private void ScanGamesDirectory()
+    public override async Task StopAsync(CancellationToken cancellationToken)
     {
+        await serviceTimer.DisposeAsync();
         
+        await base.StopAsync(cancellationToken);
     }
 
-    List<Game> GetGamesInDir(string dir)
+    private async void ScanGamesDirectory(Object? stateInfo)
+    {
+        List<Game> foundGames = GetGamesInDir(GAMES_DIR);
+
+        foreach (Game foundGame in foundGames)
+        {
+            string gamePath = GAMES_DIR + '/' + foundGame.Name;
+            
+            List<GameFile> gameFiles = GetGameFiles(gamePath);
+            Game gameWithMetadata = await ApplyGameMetadata(foundGame);
+            
+            Console.WriteLine("Hit");
+            
+        }
+
+    }
+
+    private List<Game> GetGamesInDir(string dir)
     {
         List<Game> foundGames = new List<Game>();
         
@@ -42,7 +65,7 @@ public class GameDiscoveryService : BackgroundService, IDisposable
         return foundGames;
     }
 
-    List<GameFile> GetGameFiles(string dir)
+    private List<GameFile> GetGameFiles(string dir)
     {
         List<GameFile> foundGameFiles = new List<GameFile>();
 
@@ -57,5 +80,22 @@ public class GameDiscoveryService : BackgroundService, IDisposable
         }
 
         return foundGameFiles;
+    }
+
+    private async Task<Game?> ApplyGameMetadata(Game game)
+    {
+        long? gameIgdbId = await igdbManager.GetGameIdAsync(game.Name);
+
+        if (!gameIgdbId.HasValue)
+        {
+            return null;
+        }
+
+        game.IgdbId = gameIgdbId;
+
+        game.Summary = await igdbManager.GetGameSummaryAsync(gameIgdbId.Value);
+        game.CoverUrl = await igdbManager.GetCoverUrlAsync(gameIgdbId.Value);
+
+        return game;
     }
 }
