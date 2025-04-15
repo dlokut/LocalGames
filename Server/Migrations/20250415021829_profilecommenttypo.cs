0a6ae02826ee8e3c9bd790ca0b5b905717fee60c
dlokut <dlokutijevski8@gmail.com>
2025-04-15 03:19:22 +0100

    Fixed typo in profile comments

---
+++ Server/Migrations/20250415021829_profilecommenttypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class profilecommenttypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "tId",
+                table: "Comments",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Comments",
+                newName: "tId");
+        }
+    }
+}
