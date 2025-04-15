86e7b94df2e9a3fa490437de16d5f0977cfd73ea
dlokut <dlokutijevski8@gmail.com>
2025-04-15 02:52:33 +0100

    Made post profile pic method

--- Server/Controllers/ProfileController.cs
+++ Server/Controllers/ProfileController.cs
@@ -13,6 +13,8 @@ namespace Server.Controllers;
 public class ProfileController : Controller
 {
     private const string PROFILE_PICS_DIR = "ProfilePics";
+    
+    private const string BACKGROUND_PICS_DIR = "BackgroundPics";
 
     private readonly string[] _ACCEPTED_BACKGROUND_PROFILE_PIC_EXTENSIONS =
     [
@@ -41,7 +43,7 @@ public class ProfileController : Controller
         User? currentUser = await _userManager.GetUserAsync(HttpContext.User);
         if (currentUser == null)
         {
-            return BadRequest("Must be signed in to upload playtimes");
+            return BadRequest("Must be signed in to upload profile pic");
         }
 
         string picExtension = Path.GetExtension(profilePic.FileName);
@@ -62,6 +64,34 @@ public class ProfileController : Controller
         return Ok();
     }
 
+    [HttpPost]
+    [Route("v1/PostBackgroundPic")]
+    public async Task<IActionResult> PostBackgroundPicAsync(IFormFile backgroundPic)
+    {
+         User? currentUser = await _userManager.GetUserAsync(HttpContext.User);
+         if (currentUser == null)
+         {
+             return BadRequest("Must be signed in to upload background pic");
+         }
+ 
+         string picExtension = Path.GetExtension(backgroundPic.FileName);
+         if (!_ACCEPTED_BACKGROUND_PROFILE_PIC_EXTENSIONS.Contains(picExtension))
+         {
+             return BadRequest("File must be of type " + String.Join(", ", 
+                 _ACCEPTED_BACKGROUND_PROFILE_PIC_EXTENSIONS));
+         }
+ 
+         string backgroundPicFilePath = Path.Combine(BACKGROUND_PICS_DIR, backgroundPic.FileName);
+         using FileStream fileStream = new FileStream(backgroundPicFilePath, FileMode.Create);
+         await backgroundPic.CopyToAsync(fileStream);
+ 
+         currentUser.BackgroundPicFileName = backgroundPic.FileName;
+         _dbContext.Users.Update(currentUser);
+         await _dbContext.SaveChangesAsync();
+         
+         return Ok();       
+    }
+
     [HttpGet]
     [Route("v1/GetProfilePic")]
     public async Task<IActionResult> GetProfilePicAsync(string userId)
@@ -78,9 +108,10 @@ public class ProfileController : Controller
         
         string profilePicFilePath = Path.Combine(PROFILE_PICS_DIR, profilePicFileName);
 
-        Response.Headers.ContentDisposition = "attachment";
         return Ok(new FileStream(profilePicFilePath, FileMode.Open, FileAccess.Read));
     }
+    
+    
     
     
     
