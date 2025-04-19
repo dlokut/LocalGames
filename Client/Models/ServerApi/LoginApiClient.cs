48ec09689b02e995b64fc8145228ef8cdad170b3
dlokut <dlokutijevski8@gmail.com>
2025-04-19 06:09:41 +0000

    Added login api method, with method to register

---
+++ Client/Models/ServerApi/LoginApiClient.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Client.Models.ServerApi;
+
+public class LoginApiClient
+{
+    private string LOGIN_ENDPOINT = "User/v1/PostLogin";
+    public async Task<bool> LoginAsync(string serverAddress, string username, string password)
+    {
+        HttpClient httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri(serverAddress)
+        };
+
+        string endpoint = $"{LOGIN_ENDPOINT}?username={username}&password={password}";
+        using HttpResponseMessage response = await httpClient.PostAsync(endpoint, null);
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+        
+        return true;
+    }
+    
+        private string REGISTER_ENDPOINT = "User/v1/PostRegister";
+        public async Task<bool> RegisterAsync(string serverAddress, string username, string password)
+        {
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(serverAddress)
+            };
+    
+            string endpoint = $"{REGISTER_ENDPOINT}?username={username}&password={password}";
+            using HttpResponseMessage response = await httpClient.PostAsync(endpoint, null);
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            
+            return true;
+        }
+
+
+    public async Task Test()
+    {
+        await Task.Delay(1000);
+    }
+}
