fc5d8fa96e35ed444def59fcb4c82dc58dc2214a
dlokut <dlokutijevski8@gmail.com>
2025-04-22 00:30:45 +0000

    Made method to get save files in roaming local and documents

--- Client/Models/SaveFileManager.cs
+++ Client/Models/SaveFileManager.cs
@@ -6,13 +6,47 @@ namespace Client.Models;
 
 public class SaveFileManager
 {
-    public const string ROAMING_APPDATA_PATH = "drive_c/users/steamuser/AppData/Roaming";
+    private const string ROAMING_APPDATA_PATH = "drive_c/users/steamuser/AppData/Roaming";
+    private readonly List<string> ROAMING_APPDATA_EXCLUDED_FOLDERS = 
+    [
+        "Microsoft"
+    ];
     
-    public const string LOCAL_APPDATA_PATH = "drive_c/users/steamuser/AppData/Local";
+    private const string LOCAL_APPDATA_PATH = "drive_c/users/steamuser/AppData/Local";
+    private readonly List<string> LOCAL_APPDATA_EXCLUDED_FOLDERS = 
+    [
+        "Temp",
+        "Microsoft"
+    ];
     
-    public const string DOCUMENT_PATH = "drive_c/users/steamuser/Documents";
+    private const string DOCUMENTS_PATH = "drive_c/users/steamuser/Documents";
+    private readonly List<string> DOCUMENTS_EXCLUDED_FOLDERS = 
+    [
+        "Downloads",
+        "Music",
+        "Pictures",
+        "Templates",
+        "Videos"
+    ];
 
-    public List<string> FindNewFiles(string path, List<string> excludedFolders, List<string> alreadyFoundFiles)
+    public List<string> FindSaveFiles(string prefixPath)
+    {
+        List<string> newSaveFiles = new List<string>();
+        
+        string prefixLocalAppdataPath = Path.Combine(prefixPath, LOCAL_APPDATA_PATH);
+        newSaveFiles.AddRange(FindFilesInPath(prefixLocalAppdataPath, LOCAL_APPDATA_EXCLUDED_FOLDERS));
+        
+        string prefixRoamingAppdataPath = Path.Combine(prefixPath, ROAMING_APPDATA_PATH);
+        newSaveFiles.AddRange(FindFilesInPath(prefixRoamingAppdataPath, ROAMING_APPDATA_EXCLUDED_FOLDERS));
+        
+        string prefixDocumentsPath = Path.Combine(prefixPath, DOCUMENTS_PATH);
+        newSaveFiles.AddRange(FindFilesInPath(prefixDocumentsPath, DOCUMENTS_EXCLUDED_FOLDERS));
+
+        return newSaveFiles;
+
+    }
+
+    private List<string> FindFilesInPath(string path, List<string> excludedFolders)
     {
         List<string> foundFiles = new List<string>();
         
@@ -23,11 +57,7 @@ public class SaveFileManager
             string lastFolderName = Path.GetFileName(dir);
             if (excludedFolders.Contains(lastFolderName)) continue;
 
-            foreach (string filePath in Directory.GetFiles(dir, ALL_FILES,
-                         SearchOption.AllDirectories))
-            {
-                if (!alreadyFoundFiles.Contains(filePath)) foundFiles.Add(filePath);
-            }
+            foundFiles.AddRange(Directory.GetFiles(dir, ALL_FILES, SearchOption.AllDirectories));
         }
 
         return foundFiles;
