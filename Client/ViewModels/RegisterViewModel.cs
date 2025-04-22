b1eab640105d6554b051f8397e2a207fe48ad8b7
dlokut <dlokutijevski8@gmail.com>
2025-04-22 11:37:37 +0000

    Added register view with back arrow png

--- Client/ViewModels/RegisterViewModel.cs
+++ Client/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,20 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Client.ViewModels;
 
-public class RegisterViewModel : ViewModelBase
+public partial class RegisterViewModel : ViewModelBase
 {
+    [ObservableProperty] private string _serverAddress;
     
+    [ObservableProperty] private string _username;
+    
+    [ObservableProperty] private string _password;
+
+    [RelayCommand]
+    public void GoBackToWelcome()
+    {
+        MainWindowViewModel.SwitchViews(new WelcomeViewModel());
+    }
+
 }
