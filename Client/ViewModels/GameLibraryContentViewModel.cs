398b3a486d9aed3207111d73e147d78338cd069f
dlokut <dlokutijevski8@gmail.com>
2025-04-23 07:14:09 +0000

    Added playtime to library content view

--- Client/ViewModels/GameLibraryContentViewModel.cs
+++ Client/ViewModels/GameLibraryContentViewModel.cs
@@ -1,13 +1,21 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.Media.Imaging;
+using Client.Database;
+using Client.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Client.ViewModels;
 
 public partial class GameLibraryContentViewModel : ViewModelBase
 {
+    private readonly Guid _gameId;
+
     [ObservableProperty] private string _fillerText =
         "Game summary | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum";
 
@@ -17,17 +25,56 @@ public partial class GameLibraryContentViewModel : ViewModelBase
 
     [ObservableProperty] private Bitmap _coverBitmap;
 
-    public GameLibraryContentViewModel(string gameName, string gameSummary, string coverUrl)
+    [ObservableProperty] private string _playtime;
+
+    [ObservableProperty] private List<Bitmap> _artworksBitmaps = new List<Bitmap>();
+
+    public GameLibraryContentViewModel(Guid gameId, string gameName, string gameSummary, string coverUrl,
+        int playtimeMins)
     {
+        _gameId = gameId;
+        
         GameName = gameName;
         GameSummary = gameSummary;
+        SetPlaytime(playtimeMins);
 
         _ = LoadCover(coverUrl);
+        //_ = LoadArtworks(artworkUrls);
+    }
+
+    [RelayCommand]
+    private async Task PlayGame()
+    {
+        ProtonManager protonManager = new ProtonManager();
+        await protonManager.LaunchGame(_gameId);
     }
 
     private async Task LoadCover(string url)
     {
         CoverBitmap = await LoadBitmapFromUrl(url);
+    }
+
+    private void SetPlaytime(int playtimeMins)
+    {
+        if (playtimeMins == 0) return;
+        
+        int hours = playtimeMins / 60;
+        int mins = playtimeMins % 60;
+
+        string playtimeString = "Playtime: ";
+
+        if (hours > 0) playtimeString += $"{hours} Hours ";
+        if (mins > 0) playtimeString += $"{mins} Minutes";
+
+        Playtime = playtimeString;
+    }
+
+    private async Task LoadArtworks(List<string> urls)
+    {
+        foreach (string url in urls)
+        {
+            ArtworksBitmaps.Add(await LoadBitmapFromUrl(url));
+        }
     }
 
     private async Task<Bitmap> LoadBitmapFromUrl(string url)
