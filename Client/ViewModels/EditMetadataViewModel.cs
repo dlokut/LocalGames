1b1f5bdd09881c32038a4e1772cef6bdda89c028
dlokut <dlokutijevski8@gmail.com>
2025-04-24 10:51:37 +0000

    Added upload metadata and set game title methods to edit metadata

--- Client/ViewModels/EditMetadataViewModel.cs
+++ Client/ViewModels/EditMetadataViewModel.cs
@@ -16,10 +16,21 @@ public partial class EditMetadataViewModel : ViewModelBase
 
     [ObservableProperty] private ServerGame _serverGame;
 
+    [ObservableProperty] private string _gameTitle;
+
     public EditMetadataViewModel(ServerGame serverGame)
     {
         _serverGame = serverGame;
         CoverPreviewSource = serverGame.CoverUrl;
+        
+        SetGameTitle(serverGame.Name);
+    }
+
+    [RelayCommand]
+    private async Task UploadMetadata()
+    {
+        GameApiClient gameApiClient = new GameApiClient();
+        await gameApiClient.UploadMetadataAsync(ServerGame);
     }
     
     partial void OnCoverPreviewSourceChanged(string? value)
@@ -35,5 +46,10 @@ public partial class EditMetadataViewModel : ViewModelBase
         byte[] coverPreviewBytes = await httpClient.GetByteArrayAsync(CoverPreviewSource);
 
         CoverPreviewBitmap = new Bitmap(new MemoryStream(coverPreviewBytes));
+    }
+
+    private void SetGameTitle(string gameName)
+    {
+        GameTitle = $"{gameName} - Edit metadata";
     }
 }
