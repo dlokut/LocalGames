80eafd307eaaa034434694c46d098535763b2638
dlokut <dlokutijevski8@gmail.com>
2025-04-24 11:32:01 +0000

    Made add button and files list work in upload game

--- Client/ViewModels/UploadGameViewModel.cs
+++ Client/ViewModels/UploadGameViewModel.cs
@@ -1,6 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Client.ViewModels;
 
 public partial class UploadGameViewModel : ViewModelBase
 {
-    
+    [ObservableProperty] private List<string> _addedFilesDirs = new List<string>();
+
+    [ObservableProperty] private string? _inputFileDir;
+
+    [RelayCommand]
+    private void AddFileDir()
+    {
+        List<string> newList = AddedFilesDirs.ToList();
+        newList.Add(InputFileDir);
+        AddedFilesDirs = newList;
+    }
 }
