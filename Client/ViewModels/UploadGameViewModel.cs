d7c14b5caef86435a284f1d91680b640cf9c065d
dlokut <dlokutijevski8@gmail.com>
2025-04-24 11:52:23 +0000

    MAde upload button work in upload game view

--- Client/ViewModels/UploadGameViewModel.cs
+++ Client/ViewModels/UploadGameViewModel.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Client.Models.ServerApi;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -11,9 +13,13 @@ public partial class UploadGameViewModel : ViewModelBase
 
     [ObservableProperty] private string? _inputFileDir;
 
+    [ObservableProperty] private string _gameName;
+
     [RelayCommand]
     private void AddFileDir()
     {
+        if (InputFileDir == null) return;
+        
         List<string> newList = AddedFilesDirs.ToList();
         newList.Add(InputFileDir);
         AddedFilesDirs = newList;
@@ -26,5 +32,14 @@ public partial class UploadGameViewModel : ViewModelBase
         {
             MainWindowViewModel = this.MainWindowViewModel
         });
+    }
+
+    [RelayCommand]
+    private async Task UploadGame()
+    {
+        GameApiClient gameApiClient = new GameApiClient();
+        await gameApiClient.UploadGameAsync(GameName, AddedFilesDirs);
+        
+        GoToGameLibrary();
     }
 }
